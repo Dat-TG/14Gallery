ce1ad422d7a5bc2badd937f8f16f8e0c8a375cd0
Dat-TG <dat13102k2@gmail.com>
2022-09-30 21:58:46 +0700

    Create Bottom Navigation Bar And Fragments

---
+++ app/src/main/java/com/example/a14gallery_photoandalbumgallery/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.a14gallery_photoandalbumgallery;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.a14gallery_photoandalbumgallery.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new ImageFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.image_icon:
+                    replaceFragment(new ImageFragment());
+                    break;
+                case R.id.album_icon:
+                    replaceFragment(new AlbumFragment());
+                    break;
+                case R.id.image_search_icon:
+                    replaceFragment(new ImageSearchFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        //fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
+
